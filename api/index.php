28617baba81453923b8e503147bf719c677c5987
salvacam <salvacams@gmail.com>
2016-02-21 00:03:08 +0100

    refactorizacion codigo php, uso de herramienta phpmd

--- api/index.php
+++ api/index.php
@@ -1,52 +1,79 @@
 <?php
-
+ 
+ /**
+ * Api para todo_list
+ * 
+ * PHP version 5
+ * 
+ * @category MyCategory
+ * @package  MyPackage
+ * @author   Salvador Camacho <salvacams@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://salvacam.tk/todo_list/api/index.php 
+ */
+ 
+// define la zona horaria
 date_default_timezone_set('Europe/Madrid');
 
+// requerimientos de archivos
 require 'src/Nanite.php';
 require 'src/functions.php';
 
-Nanite::get('/', function() {
-    header("Content-Type: application/json");
-    echo listar();
-});
 
-Nanite::get('/delete/([a-zA-Z0-9]+)', function($id) {
-	header("Content-Type: application/json");
-	if ($id == 'All') {
-		echo borrarAll();
-	} else if ($id == 'Active' || $id == 'Completed') {
-		echo borrarEstado($id);
-	} else {
-		echo borrar($id);
-	}
-});
+Nanite::get(
+    '/', function () {        
+        header("Content-Type: application/json");
+        echo listar();
+    }
+);
 
-Nanite::post('/new', function() {
-	header("Content-Type: application/json");
-	$objDatos = json_decode(file_get_contents("php://input"));
-    $texto = '';
-    if (isset($objDatos->texto)) {
-        $texto = $objDatos->texto;
-    }
-    echo crear($texto);
-});
 
-Nanite::post('/update/([0-9]+)', function($id) {
-    header("Content-Type: application/json");
-    $objDatos = json_decode(file_get_contents("php://input"));
-    $texto = '';
-    if (isset($objDatos->texto)) {
-        $texto = $objDatos->texto;
+Nanite::get(
+    '/delete/([a-zA-Z0-9]+)', function ($id) {
+        header("Content-Type: application/json");
+        if ($id == 'All') {
+            echo borrarAll();
+        } else if ($id == 'Active' || $id == 'Completed') {
+            echo borrarEstado($id);
+        } else {
+            echo borrar($id);
+        }
     }
-    $estado = '';
-    if (isset($objDatos->estado)) {
-        $estado = $objDatos->estado;
+);
+
+
+Nanite::post(
+    '/new', function () {
+        header("Content-Type: application/json");
+        $objDatos = json_decode(file_get_contents("php://input"));
+        $texto = '';
+        if (isset($objDatos->texto)) {
+            $texto = $objDatos->texto;
+        }
+        echo crear($texto);
     }
-    echo modificar($id, $texto, $estado);
-});
+);
+
+
+Nanite::post(
+    '/update/([0-9]+)', function ($id) {
+        header("Content-Type: application/json");
+        $objDatos = json_decode(file_get_contents("php://input"));
+        $texto = '';
+        if (isset($objDatos->texto)) {
+            $texto = $objDatos->texto;
+        }
+        $estado = '';
+        if (isset($objDatos->estado)) {
+            $estado = $objDatos->estado;
+        }
+        echo modificar($id, $texto, $estado);
+    }
+);
+
 
 if (!Nanite::$routeProccessed) {
     header('HTTP/1.1 405 Method Not Allowed');
     header("Content-Type: text/html");
     echo info();
-} 
+}
--- api/src/functions.php
+++ api/src/functions.php
@@ -1,125 +1,201 @@
 <?php
 
+/**
+ * Funciones para el trabajo con el modelo
+ * 
+ * PHP version 5
+ * 
+ * @category MyCategory
+ * @package  MyPackage
+ * @author   Salvador Camacho <salvacams@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://salvacam.tk/todo_list/api/src/functions.php 
+ */
+ 
+ // requerimientos de archivos
 require 'idiorm.php';
 
-
+// parametros de conexíon con la base de datos
 $host = 'localhost';
 $database = 'todo_list';
 $user = 'user';
 $pass = 'pass';
 
+// nombre de la tabla donde se encuentran las tareas
 const TABLA = 'todo_task';
 
+// configuración del orm
 ORM::configure('mysql:host='.$host.';dbname='.$database);
 ORM::configure('username', $user);
 ORM::configure('password', $pass);  
 
-function listar() {
-	
-	$lista = ORM::for_table(TABLA)->find_many();
-	$salida = [];
-	foreach ($lista as $task) {
-	    $id = $task->id;
-	    $texto = $task->texto;
-	    $estado = $task->estado;
-	    $salida[$id] = array('id'=>$id, 'texto'=>$texto, 'estado'=>$estado);
-	    //$salida[] = array('id'=>$id, 'texto'=>$texto, 'estado'=>$estado);
-	}
-	if ( count($salida) == 0 ) {
-		return json_encode(-1);
-	}	
-	return json_encode($salida);
+
+/**
+ * Muestra todas las tareas
+ * 
+ * @return json
+ */
+function listar() 
+{
+
+    $lista = ORM::for_table(TABLA)->find_many();
+    $salida = [];
+    foreach ($lista as $task) {
+        $id = $task->id;
+        $texto = $task->texto;
+        $estado = $task->estado;
+        $salida[$id] = array('id'=>$id, 'texto'=>$texto, 'estado'=>$estado);
+        //$salida[] = array('id'=>$id, 'texto'=>$texto, 'estado'=>$estado);
+    }
+    if (count($salida) == 0) {
+        return json_encode(-1);
+    }
+    return json_encode($salida);
 }
 
 
-function borrarAll() {
-	try {
-		$lista = ORM::for_table(TABLA)
-		->find_many();
-		foreach ($lista as $task) {
-			$task->delete();	
-		}		    	
+/**
+ * Borra todas las tareas
+ * 
+ * @return json
+ */
+function borrarAll() 
+{
+    
+    try {
+        $lista = ORM::for_table(TABLA)
+        ->find_many();
+        foreach ($lista as $task) {
+            $task->delete();
+        }
 
-		$salida = listar();
-		return json_encode($salida);
-	} catch (Exception $e) {		
-		return json_encode(0);
-	}	
+        $salida = listar();
+        return json_encode($salida);
+    } catch (Exception $e) {
+        return json_encode(0);
+    }
 }
 
 
-function borrarEstado($remove) {	
-	if ( $remove == 'Active' || $remove == 'Completed') {	
-		if ( $remove == 'Active' ) {
-			$estado = 0;
-		} else if ($remove == 'Completed') {
-			$estado = 1;
-		}
-		try {
-			$lista = ORM::for_table(TABLA)
-		    ->where('estado', $estado)
-		    ->find_many();		    
-		    foreach ($lista as $task) {
-		    	$task->delete();	
-			}			
+/**
+ * Borra todas las tareas según el estado
+ * 
+ * @param string $remove Estado de las tareas a borrar
+ * 
+ * @return json
+ */
+function borrarEstado($remove) 
+{
 
-			$salida = listar();
-			return json_encode($salida);
-		} catch (Exception $e) {		
-			return json_encode(0);
-		}
-	}
+    if ($remove == 'Active' || $remove == 'Completed') {
+        if ($remove == 'Active') {
+            $estado = 0;
+        } else if ($remove == 'Completed') {
+            $estado = 1;
+        }
+        
+        try {
+            $lista = ORM::for_table(TABLA)
+            ->where('estado', $estado)
+            ->find_many();
+        
+            foreach ($lista as $task) {
+                $task->delete();
+            }
+
+            $salida = listar();
+            return json_encode($salida);
+        } catch (Exception $e) {
+            return json_encode(0);
+        }
+    }
+    
+    return json_encode(0);
 }
 
-function borrar($remove) {
-	try {
-		ORM::for_table(TABLA)
-		->where('id', $remove)
-		->find_one()
-		->delete();				
 
-		$salida = listar();
-		return json_encode($salida);
-	} catch (Exception $e) {	
-		return json_encode(0);
-	}	
+/**
+ * Borra una tarea
+ * 
+ * @param int $remove Identificador de la tarea
+ * 
+ * @return json
+ */
+function borrar($remove)
+{
+    
+    try {
+        ORM::for_table(TABLA)
+        ->where('id', $remove)
+        ->find_one()
+        ->delete();
+
+        $salida = listar();
+        return json_encode($salida);
+    } catch (Exception $e) {
+        return json_encode(0);
+    }
 }
 
-function crear($texto) {
-	$salida = 0;
-	try {
-		$task = ORM::for_table(TABLA)->create();
-		$task->texto = $texto;
-		$task->estado = 0;
-		$task->save();
-		
-		//$salida = 1;		
 
-		$salida = listar();
-	} catch (Exception $e) {		
-	}	
-	return $salida;
+/**
+ * Crea una tarea
+ * 
+ * @param string $texto nombre de la tarea
+ * 
+ * @return json
+ */
+function crear($texto) 
+{
+    $salida = 0;
+    try {
+        $task = ORM::for_table(TABLA)->create();
+        $task->texto = $texto;
+        $task->estado = 0;
+        $task->save();
+
+        $salida = listar();
+    } catch (Exception $e) {
+    }
+    return $salida;
 }
 
-function modificar($identificador, $texto, $estado) {
-	$salida = 0;
-	try {
-		$task = ORM::for_table(TABLA)->where('id', $identificador)->find_one();
-		$task->texto = $texto;
-		$task->estado = $estado;
-		$task->save();
 
-		//$salida = 1;
+/**
+ * Modifica una tarea
+ * 
+ * @param int    $identificador Identificador de la tarea
+ * @param string $texto         Nuevo texto de la tarea
+ * @param int    $estado        estado de la tarea
+ * 
+ * @return json
+ */
+function modificar($identificador, $texto, $estado)
+{
+    
+    $salida = 0;
+    try {
+        $task = ORM::for_table(TABLA)->where('id', $identificador)->find_one();
+        $task->texto = $texto;
+        $task->estado = $estado;
+        $task->save();
 
-		$salida = listar();
+        $salida = listar();
 
-	} catch (Exception $e) {		
-	}	
-	return $salida;
+    } catch (Exception $e) {
+    }
+    return $salida;
 }
 
-function info(){
-	return 'GET / : List all task<br/>GET /delete/{id | all | active | commpleted} : Remove task(s)<br/>'.
-	'POST /new Request (texto): Create task<br/>'.
-	'POST /update/{id} Request (texto, estado): Update task<br/>';
+/**
+ * Devuelve la forma de uso de la api
+ * 
+ * @return string
+ */
+function info()
+{
+    return 'GET / : List all task<br/>GET /delete/{id | all | active |'.
+    ' commpleted} : Remove task(s)<br/>'.
+    'POST /new Request (texto): Create task<br/>'.
+    'POST /update/{id} Request (texto, estado): Update task<br/>';
 }
